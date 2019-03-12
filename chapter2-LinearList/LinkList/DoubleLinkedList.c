f802e5d9a4bc97c79537833842a1d7596c59097a
gerrit <aimee.yang@mobilecarlink.com>
2019-03-12 12:48:18 +0800

    add double linked list

---
+++ chapter2-LinearList/LinkList/DoubleLinkedList.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct DuLNode{
+	int data;
+	struct DuLNode * prev;
+	struct DuLNode * next;
+
+}DuLNode, *DuLNodeList;
+
+
+int mai()
+{
+	return 0;
+}
